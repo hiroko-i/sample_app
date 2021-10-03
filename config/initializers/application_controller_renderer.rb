86b9f16858332db0cca0b05f3c5fb08a63bd83b5
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2021-10-03 06:32:11 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e84a0b70e9f30a78b0eb5ff68131ea6c7199f5f438132bec41ca6387f0d86f80bb4a5963449f4ea8206cb3671d386a72aa383e61ee8ab4ab60ceb6e7c7d3a249'
